a17bbf8fb93ed929124ff23944f7c0105d7f9d70
delvin5 <delvin.guzman@upr.edu>
2015-10-31 14:43:29 -0400

    First query to Heroku's DB.

--- public/angular/controllers/friendsController.js
+++ public/angular/controllers/friendsController.js
@@ -3,9 +3,15 @@
 
     app.controller('friendsController', ['$http',function($http){
         var friendsObj = this;
-        friendsObj.friends = [];
-        $http.get('/jsondata/friends.json').success(function (data) {
+        friendsObj.friends = {};
+
+        $http.get('/query/friends').success(function (data) {
+            console.log("\n\n*** Data: " + data);
             friendsObj.friends=data;
         });
+
+        //$http.get('/jsondata/friends.json').success(function (data) {
+        //    friendsObj.friends=data;
+        //});
     }]);
 })();
--- routes/query.js
+++ routes/query.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/allUsers', function(req, res, next) {
-  res.send('respond with a resource');
+var path = require('path');
+var pg = require('pg');
+
+var connectionString = require(path.join(__dirname, '../', 'config'));
+
+var QUERY_FRIENDS =
+    "SELECT first_name, last_name, email, birthday" +
+    " FROM tonight.users;";
+
+
+
+router.get('/friends', function(req, res) {
+
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        //console.log("\n\n** 1");
+        if(err) {
+            done();
+            console.log(err);
+            return res.status(500).json({ success: false, data: err});
+        }
+
+
+        // SQL Query > Select Data
+        var query = client.query(QUERY_FRIENDS);
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+            console.log(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+
+    });
+
 });
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
