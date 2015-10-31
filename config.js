2cf6b898f460e9a744ad59333be2bcdf6201d350
delvin5 <delvin.guzman@upr.edu>
2015-10-31 00:52:08 -0400

    First query to Heroku's DB.

---
+++ config.js
@@ -0,0 +1,7 @@
+//var connectionString = process.env.DATABASE_URL ||
+//                        'postgres://postgres:password@localhost:5432/postgres';
+
+var connectionString = process.env.DATABASE_URL ||
+    'postgres://lnyngrsjqxuoux:y7mlmTM_Qo4d-va0SUXiAEUHm5@ec2-54-83-51-0.compute-1.amazonaws.com:5432/d9n9dj3cst5g7e?ssl=true';
+
+module.exports = connectionString;
